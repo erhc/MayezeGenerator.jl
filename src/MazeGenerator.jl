b95561dae7e86210711f494ba4c5adb35b653e11
erhc <hodzic.e.k@outlook.com>
2023-02-08 18:33:41 +0100

    Comment

--- src/MazeGenerator.jl
+++ src/MazeGenerator.jl
@@ -7,6 +7,7 @@ export generateMaze, plotMaze
 
 
 function generateMaze(height::Int, width::Int, start::Tuple{Int, Int}=(1, 1))
+    # This function will take a string as an argument once there are other methods to call
     return randomizedDFS(height, width, start)
 end
 
